d274e8bdd2856c379d8ff87022d5dd6821ee0ebd
sksmssk4 <tlvmak7@naver.com>
2017-04-25 23:24:30 +0900

    Add UI function
    Add Start, Exit, Stop

---
+++ Assets/Exit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Application.Quit(); // 애플리케이션 종료
+        }
+		
+	}
+}
---
+++ Assets/exit3.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Exit3 : MonoBehaviour
+{
+    void Start() { }
+    void Update() { }
+    void Quit()
+    {
+        Application.Quit();
+    }
+}
