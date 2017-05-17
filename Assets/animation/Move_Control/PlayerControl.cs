81df54424588811b22d75693b5aa248238a67187
sksmssk4 <tlvmak7@naver.com>
2017-05-18 01:23:39 +0900

    Restrict Jump Count(Developing)

--- Assets/animation/Move_Control/PlayerControl.cs
+++ Assets/animation/Move_Control/PlayerControl.cs
@@ -6,60 +6,110 @@ public class PlayerControl : MonoBehaviour {
 
     Animator animator;
     Rigidbody rb;
-
+    public GameObject KuriKo;
+    public GameObject CreateBox; // prefab
+    public GameObject CreateBoxPosition;
+    public float health = 1; // 체력
+    
     float directionX = 0;
     float directionY = 0;
     bool walking = false;
-    float Speed = 2.0f; // 걷는 속도
-    float J_P = 7.0f; // 점프력
+    float Speed = 8.0f; // 걷는 속도
+    float J_P = 15.0f; // 점프력
+    bool jumping = false;
 
+    bool death;
+
+
+
+    void Start()
+    {
+        death = false;
+    }
 	// Use this for initialization
-	void Start () {
+	void Awake () {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();  // rigidbody에서 지원하는 AddForce를 사용하기 위해
     }
 
-	// Update is called once per frame
-	void Update () {
-        if(animator)
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (death == true) // 죽었을때 동작 정지
         {
-            float h = Input.GetAxis("Horizontal");
-            float v = Input.GetAxis("Vertical");
-            walking = true;
-            if(h>0)
+            return;
+        }
+
+        else // 죽지않았을경우 (death == false)
+        {
+
+            if (animator)
             {
-                directionX = 1;
-                directionY = 0;
+                float h = Input.GetAxis("Horizontal");
+                float v = Input.GetAxis("Vertical");
+                walking = true;
+                if (h > 0)
+                {
+                    directionX = 1;
+                    directionY = 0;
+                }
+                else if (h < 0)
+                {
+                    directionX = -1;
+                    directionY = 0;
+                }
+                //else if(v>0)
+                //{
+                //    directionX = 0;
+                //    directionY = 1;
+                //}
+
+                else
+                {
+                    walking = false;
+                }
+
+                if (walking)
+                {
+                    transform.Translate(new Vector3(directionX, 0.0f, 0.0f) * Time.deltaTime * Speed);
+                }
+
+                animator.SetFloat("DirectionX", directionX);
+                animator.SetFloat("DirectionY", v);
+                animator.SetBool("Walking", walking);
             }
-            else if(h<0)
-            {
-                directionX = -1;
-                directionY = 0;
+       
+             if (jumping == false && Input.GetKeyDown(KeyCode.Space) ) // 스페이스를 Down(누르면)하고 Jumping == false일 떄만 점프동작
+             {
+                jumping = true;
+                rb.AddForce(Vector3.up * J_P, ForceMode.VelocityChange); // 위쪽방향으로 J_P값만큼 점프한다.
             }
-            //else if(v>0)
-            //{
-            //    directionX = 0;
-            //    directionY = 1;
-            //}
-            
-            else
+             
+            if(rb.velocity.y == 0)
             {
-                walking = false;
+                jumping = false;
             }
 
-            if(walking)
+            Debug.Log("jumping : " + jumping);
+
+            if (Input.GetKeyDown(KeyCode.Z))
             {
-                transform.Translate(new Vector3(directionX, 0.0f, 0.0f) * Time.deltaTime * Speed);
+                GameObject BoxPosition = (GameObject)Instantiate (CreateBox); // Z키를 누르면 박스를 생성
+                BoxPosition.transform.position = CreateBoxPosition.transform.position; 
             }
 
-            animator.SetFloat("DirectionX", directionX);
-            animator.SetFloat("DirectionY", v);
-            animator.SetBool("Walking", walking);
-        }	
-        
-        if (Input.GetKeyDown(KeyCode.Space)) // 스페이스를 Down(누르면)하면 동작
+
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
         {
-            rb.AddForce(Vector3.up*J_P, ForceMode.Impulse); // 위쪽방향으로 J_P값만큼 점프한다.
-        }	
-	}
+            animator.SetBool("Dies", true);
+            death = true;
+        }
+    }
 }
