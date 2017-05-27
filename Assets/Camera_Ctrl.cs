a63072fe063f43859f9961b32df2eb6a4e09ce17
sksmssk4 <tlvmak7@naver.com>
2017-05-28 02:04:19 +0900

    Other Thorn collision+Map Design

--- Assets/Camera_Ctrl.cs
+++ Assets/Camera_Ctrl.cs
@@ -19,7 +19,8 @@ public class Camera_Ctrl : MonoBehaviour {
         // NewPosition =  kuriko.transform.position + new Vector3(0.0f, 2.9f, -2.6f);
 
 
-        transform.position = new Vector3(Mathf.Clamp(kuriko.transform.position.x, -53.5f, 4.3f), Mathf.Clamp(kuriko.transform.position.y, -18.5f, 7.0f) + 2.9f, kuriko.transform.position.z - 2.6f);
+        transform.position = new Vector3(Mathf.Clamp(kuriko.transform.position.x, -51.5f, 2.3f), // x축으로 -53.5f이하 4.3f이상 카메라 못움직이게
+            Mathf.Clamp(kuriko.transform.position.y, -18.5f, 7.0f) + 2.9f, kuriko.transform.position.z - 2.6f);
 
     }
 }
